9ded8a89cbf8853aee060c44b734e59b8e7f008e
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-01 01:23:26 +0900

    add validation code to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,40 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validation" do
     context "valid" do
-      it 'is valid with email ans password, display name' do
+      it "is valid with email ans password, display name" do
         
       end
     end
 
     context "invalid" do
-      it 'is invalid without email' do
+      it "is invalid without email" do
         
       end
 
-      it 'is invalid without password' do
+      it "is invalid without password" do
         
       end
 
-      it 'is invalid without display name' do
+      it "is invalid without display name" do
         
       end
+
+      it "is invalid with password shorter than 8" do
+
+      end
+
+      it "is invalid with passwor that does'nt include large character" do
+        
+      end
+
+      it "is invalid with passwor that does'nt include small character" do
+        
+      end
+
+      it "is invalid with passwor that does'nt include number" do
+        
+      end
+
     end
   end
 end
