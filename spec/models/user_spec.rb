60420db2ac9f7a6abac8528230ec0d2174145238
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-01 01:12:31 +0900

    write validation test code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    context "valid" do
+      it 'is valid with email ans password, display name' do
+        
+      end
+    end
+
+    context "invalid" do
+      it 'is invalid without email' do
+        
+      end
+
+      it 'is invalid without password' do
+        
+      end
+
+      it 'is invalid without display name' do
+        
+      end
+    end
+  end
 end
