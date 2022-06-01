53c85b26a9de31d2777f3706cb84cd562c853a07
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-01 17:26:39 +0900

    write spec outline

--- spec/models/subtask_spec.rb
+++ spec/models/subtask_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Subtask, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) {FactoryBot.create(:user)}
+  let(:user2) {FactoryBot.create(:user)}
+  let(:task1) {FactoryBot.create(:task, user_id: user1.id, assignee_id: user2.id)}
+
+  it "belongs to task" do
+    belong_to(:task_id).class_name(Task)
+  end
+
+  describe "validation" do
+    context "valid" do
+      it "is valid with task id and description, model" do
+        subtask = FactoryBot.build
+      end
+    end
+
+    context "incalid" do
+      it "is invalid withoud description" do
+
+      end
+
+      it "is invalid without mode" do
+        
+      end
+    end
+    
+  end
+  
 end
