4e59e6230ce4562d5f4173e66ddc776f9f3fddc3
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-02 11:19:37 +0900

    pick 'write failed user api test' from user-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   #Auth endpoint
   post    '/auth',    to: 'sessions#create'
   delete  '/auth',    to: 'sessions#destroy'
+
+  #users api
+  post  "/users",    to: "users#create"
+  get   "/users",    to: "users#index"
+  patch "/users",    to: "users#update"
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,11 @@
+
 def sign_in_as(email, password)
     post "/auth", params: {
         email: user.email,
         password: user.password
     }.to_json, header: {"Content-Type" => "application/json"}
+
+def sign_up(display_name, email, password, password_confirmation)
+    post "/users", params: { display_name: display_name, email: email, password: password, password_confirmation: password_confirmation}
+
 end
