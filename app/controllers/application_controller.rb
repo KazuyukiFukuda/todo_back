c02a44e48719ad169970695a7f6093fbe8d15c4b
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-02 17:50:38 +0900

    get 'make user controller as pass test' from user-model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
+
     include SessionHelper
+
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -8,4 +8,11 @@ def sign_in_as(email, password)
 def sign_up(display_name, email, password, password_confirmation)
     post "/users", params: { display_name: display_name, email: email, password: password, password_confirmation: password_confirmation}
 
+def sign_up(user)
+    post "/users",  params: {
+        display_name: user.display_name,
+        email: user.email,
+        password: user.password,
+        password_confirmation: user.password_confirmation
+    }.to_json, headers: { "Content-Type" => "application/json" }
 end
