11856be9f21c59905eb4cc3208de3e5cece3950d
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-05 23:09:45 +0900

    fix task validation and let task and subtask guess inverse assotiation

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,5 +1,5 @@
 class Subtask < ApplicationRecord
-  belongs_to :task, foreign_key: "task_id", inverse_of: :patients, optional: true
+  belongs_to :task, foreign_key: "task_id", optional: true
   accepts_nested_attributes_for :task, allow_destroy: true
 
   validates :description, presence: true
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,12 @@
 class Task < ApplicationRecord
-    belongs_to :user,      class_name: "User", foreign_key: "user_id", inverse_of: :patients, optional: true
-    belongs_to :assignee,   class_name: "User", foreign_key: "assignee_id", inverse_of: :patients, optional: true
+    belongs_to :user,      class_name: "User", foreign_key: "user_id", optional: true
+    belongs_to :assignee,   class_name: "User", foreign_key: "assignee_id",  optional: true
     has_many :subtasks, class_name: "Subtask"
     accepts_nested_attributes_for :user, :assignee, :subtasks, update_only:true, allow_destroy: true
 
     validates :name, presence: true
-    #validate  :deadline_check
-    #validates :user_id, presence: true
+    validate  :deadline_check
+    validates :user_id, presence: true
     validates :completed, inclusion: {in: [true, false]}
     validates :public, inclusion: {in: [true, false]}
 
