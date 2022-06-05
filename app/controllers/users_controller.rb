77648b8afaf4890a7ece38fe9dc05f723295440e
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-05 22:46:47 +0900

    write PATCH /user/:id that pass test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,45 @@ class UsersController < ApplicationController
 
     def update
         if logged_in?
+            current_user
+            user_id = params[:id].to_i
+            json_request = JSON.parse(request.body.read)
 
-            render json: {message: @user}, status: 200
+            if !(User.where(id: user_id).exists?)
+                render json: {message: "このユーザーは存在しません"}, status: 404
+                return
+            else
+                user = User.find(user_id)
+            end
+
+            if current_user != User.find(user_id)
+                render json: {message: "このユーザーへのアクセス権限がありません"}, status: 401
+                return
+            end
+
+            if BCrypt::Password.new(user.password_digest) != json_request["current_password"]
+                render json: {message: "パスワードが間違っています"}, status:400
+                return
+            end
+
+            if json_request["password"] != json_request["password_confirmation"]
+                render json: {message: "新たなパスワードと確認用パスワードが異なっています"}, status:400
+                return
+            end
+
+            if json_request["password"].nil?
+                json_request["password"] = json_request["current_password"]
+            end
+
+
+            updated_user_hash = json_request.without("current_password")
+            
+            updated_user_hash.delete_if{ |_, v| v == nil }
+            hoge = updated_user_hash.symbolize_keys
+
+            hoge = user.update!(updated_user_hash)
+
+            render json: {message: hoge}, status: 200
         else
             render json: {message: "loginされていません"}, status: 401
         end
--- spec/requests/users_apis_spec.rb
+++ spec/requests/users_apis_spec.rb
@@ -72,55 +72,60 @@ RSpec.describe "UsersApis", type: :request do
 
 
   describe "PATCH /users:id" do
-    context "success" do
-      let(:user) {FactoryBot.create(:user)}
-      let(:update_email) {{
-        display_name: nil,
-        email: Faker::Internet.email,
-        old_password: nil,
-        password: nil,
-        password_confirmation: nil,
-      }}
-      let(:update_password) {{
-        display_name: nil,
-        email: nil,
-        old_password: user.password,
-        password: "AAAbbb111",
-        password_confirmation: "AAAbbb111",
-      }}
+    user = FactoryBot.create(:user, password: "BBBBbbbb3333", password_confirmation:"BBBBbbbb3333")
+    update_email = {
+      display_name: nil,
+      email: "hoge@test.com",
+      current_password: "BBBBbbbb3333",
+      password: nil,
+      password_confirmation: nil,
+    }
+    update_password = {
+      display_name: nil,
+      email: nil,
+      current_password: "BBBBbbbb3333",
+      password: "AAAbbb111",
+      password_confirmation: "AAAbbb111",
+    }
 
+    context "success" do
       before do
         sign_in_as(user.email, user.password)
       end
 
       it "update successfully" do
-        patch user_path(user.id), :params =>update_email.to_json, headers: {"Content-Type" => "application/json"}
-        expect(user.attributes["email"]).to eq(update_email[:email])
+        patch user_path(user.id), :params => update_email.to_json, headers: {"Content-Type" => "application/json"}
+        expect(User.find(user.id).email).to eq(update_email[:email])
       end
 
       it "doesn't update others" do
-        expect{
-          patch user_path(user.id), :params =>update_email.to_json, headers: {"Content-Type" => "application/json"}
-        }.to change{ user.attributes.without["email"] }
+        before_user = User.find(user.id).attributes
+
+        patch user_path(user.id), :params =>update_email.to_json, headers: {"Content-Type" => "application/json"}
+
+        after_user = User.find(user.id).attributes
+        expect(before_user.without("email", "updated_at", "created_at", "password_digest")).to eq(after_user.without("email", "updated_at", "created_at", "password_digest"))
       end
 
       it "update password successfully" do
         patch user_path(user.id), :params =>update_password.to_json, headers: {"Content-Type" => "application/json"}
-        expect(user.password).to eq(update_password[:password])
+
+        password_digest = User.find(user.id).password_digest
+        expect( BCrypt::Password.new(password_digest).is_password?(update_password[:password]) ).to eq(true)
       end
     end
 
     context "failed" do
-      let (:user) {FactoryBot.create(:user)}
+      let (:user) {FactoryBot.create(:user, password: "BBBBbbbb3333", password_confirmation:"BBBBbbbb3333")}
       let(:update_with_wrong_old_password) {{
         display_name: nil,
         email: nil,
-        old_password: "BB#{user.password}AA",
+        current_password: "BB#{user.password}AA",
         password: "AAAbbb111",
         password_confirmation: "AAAbbb111",
       }}
 
-      it "doesn't update password if old_password is wrong" do
+      it "doesn't update password if current password is wrong" do
         sign_in_as(user.email, user.password)
         patch user_path(user.id), :params => update_with_wrong_old_password.to_json, headers: {"Content-Type" => "application/json"}
         expect(response).to have_http_status(400)
@@ -129,7 +134,7 @@ RSpec.describe "UsersApis", type: :request do
       it "doesn't update if user doesn't have authorization" do
         sign_in_as(user.email, user.password)
         other_user = FactoryBot.create(:user)
-        patch user_path(other_user.id), :params => update_with_wrong_old_password.to_json, headers: {"Content-Type" => "application/json"}
+        patch user_path(1), :params => update_with_wrong_old_password.to_json, headers: {"Content-Type" => "application/json"}
 
         expect(response).to have_http_status(401)
       end
