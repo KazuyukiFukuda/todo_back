458d6e869567c6a180b61163f0aecdffe7d46b04
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-07 16:37:58 +0900

    fix CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+        origins(['localhost', /localhost:\d+\Z/, /(.+).local.example-dev.com\Z/])
+        resource '*',
+            headers: :any,
+            methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            credentials: true
     end
 end
